3d33d4c766e965fd1a941c1f22022ca7c148a6c2
SerebryakovaIA <ia.serebr@gmail.com>
2024-09-20 18:40:48 +0300

    HomeWork

---
+++ src/main/java/org/example/exception/StringNullPointerException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class StringNullPointerException extends RuntimeException{
+    public StringNullPointerException(String message) {
+        super(message);
+    }
+}
